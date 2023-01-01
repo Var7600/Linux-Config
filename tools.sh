d7b76a65b1c6cf3170ff9a4a43657d6252194fa2
doudou diawara <doudoudiawara3@gmail.com>
2022-12-31 20:17:01 -0500

    dev tools

---
+++ tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install build-essential -y 
+sudo apt install git -y  
+sudo apt install most -y  
+sudo apt install code -y
+sudo apt install cppcheck -y 
+sudo apt install cppcheck-gui -y
+#sudo apt-get install meld
