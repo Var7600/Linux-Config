2863fcbe6b99439d395624923c2582f341e33962
doudou diawara <doudoudiawara3@gmail.com>
2023-03-03 00:37:55 -0500

    add tools.sh to install script

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+#install default utilities
+./tools.sh
+
 #save current bashrc config to install new bashrc config
 mv ~/.bashrc ~/.bashrc.old && cp ./bashrc ~/.bashrc && cp bash_aliases ~/.bash_aliases
 
 #copie folder Script to HOME
 cp -R ./Script/ ~/
+
+#install git prompt bash
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 #restart shell
 source ~/.bashrc
