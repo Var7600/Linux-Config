a5cd6da62ef6ff556cf63627e60bd92704b7cb29
Var7600 <77991946+Var7600@users.noreply.github.com>
2023-06-08 21:54:18 -0400

    add mysql

--- tools.sh
+++ tools.sh
@@ -3,6 +3,7 @@
 sudo apt install build-essential -y 
 sudo apt install git -y  
 sudo apt install most -y  
+sudo apt install mysql-client mysql-server -y
 #sudo apt install code -y
 sudo apt install cppcheck -y 
 sudo apt install check -y # unit test C
