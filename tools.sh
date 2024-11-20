7ef080b832dc8c10cb91a1aecf79a5028f222447
doudou diawara <doudoudiawara3@gmail.com>
2024-11-19 22:58:16 -0500

    add bats automate testing script

--- tools.sh
+++ tools.sh
@@ -4,6 +4,7 @@ sudo apt install build-essential -y
 sudo apt install git -y  
 sudo apt install cmake -y
 sudo apt install most -y  
+sudo apt install bats -y
 sudo apt install tree -y  
 sudo apt install exuberant-ctags -y
 sudo apt install cscope -y
