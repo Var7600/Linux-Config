a8dc7fd60a90accd83d91d4ac27ad14ba444b029
doudou diawara <doudoudiawara3@gmail.com>
2024-11-19 22:53:27 -0500

    add option d for file retention

--- Scripts/rm-secure.sh
+++ Scripts/rm-secure.sh
@@ -1,7 +1,11 @@
 # Script to save files or directories when deleted
 # @original author @Christophe blaess
 # this is modified version from the orginal one author Var600(Doudou)
+
+# recovery folder path
 sauvegarde_rm=~/rm_saved/
+# default retention period is 60 days
+days=60
 
 function rm {
     local opt_force=0
@@ -11,13 +15,14 @@ function rm {
     local opt_empty=0
     local opt_list=0
     local opt_restore=0
+		local override_days=0
     local opt
 
     OPTIND=0
     # process command line argument
     while getopts ":dfirRvels-:" opt ; do
         case $opt in
-        d) ;; # ignore
+        d) override_days=$OPTARG ;; 
         f) opt_force=1 ;;
         i) opt_interactive=1 ;;
         r | R ) opt_recursive=1 ;;
@@ -34,29 +39,34 @@ function rm {
                 help) 
                     /bin/rm --help
                     echo "rm_secure:"
+										echo "-d <days> --specify retention period in days"
                     echo " -e --empty save folder"
                     echo " -l --list file(s) in the saved folder"
                     echo " -s, --restore from the save folder"
                     return 0 ;; # exit code for rm
                 version ) 
                     /bin/rm --version
-                    echo "(rm_secure 1.2)"
+                    echo "(rm_secure 1.3)"
                     return 0 ;;
                 # default option
                 empty ) opt_empty=1 ;;
                 list ) opt_list=1 ;;
                 restore) opt_restore=1 ;;
                 * ) echo "unknow option -- $OPTARG" # option invalid 
-                    return 1 ;; # erreur
+                    return 1 ;; # error 
             esac ;;
         ? ) echo "illegal option -- $OPTARG"
             return 1 ;;
         esac
     done
 
-    shift $((OPTIND - 1)) # shift n decale les arguments en supprimant les n premiers
+    shift $((OPTIND - 1)) # shifts the arguments by removing the first n
+		
+		# set retention period
+		local retention_period=${days:-$override_days}
 
-    # create the backup repo 
+
+    # create the backup directory
     if [ ! -d "$sauvegarde_rm" ]; then
         mkdir "$sauvegarde_rm"
     fi
@@ -80,21 +90,24 @@ function rm {
         done
         return
     fi
+		
+		# delete files after retention period
+		find "$sauvegarde_rm" -mindepth 1 -type f -mtime +$retention_period -exec /bin/rm -f {} \;
 
     # delete
     while [ -n "$1" ]; do
         # delete interactive mode 
         if [ $opt_force -ne 1 ] && [ $opt_interactive -ne 0 ]; then
             local response
-            echo -n "Delete $1 ?"
+            echo -n "Delete $1?[y/n]: "
             read -r response
-            if [ "$response" != 'y' ] && [ "$response" != "Y" ] && [ "$response" != "o" ] && [ "$response" != "O" ]; then
+            if [ "$response" != "y" ] && [ "$response" != "Y" ]; then
                 shift # delete
                 continue
             fi
         fi
 
-        # Les répertoires nécessitent l'option recursive
+        # for deleting a directory
         if [ -d "$1" ] && [ $opt_recursive -eq 0 ]; then
             echo "$1 is a Directory please use -r or -R option."
             shift
@@ -102,14 +115,14 @@ function rm {
         fi
 
         if [ $opt_verbose -ne 0 ]; then
-            echo "deleting $1"
-            echo "Saved in $sauvegarde_rm"
+					echo "deleting $1 (Saved in $sauvegarde_rm)"
         fi
 
-        mv -f "$1" "${sauvegarde_rm}/"
+				timestamp=$(date +%Y%m%d%H%M%S)
+				mv -f "$1" "${sauvegarde_rm}/${timestamp}_$(basename "$1")"
         shift
     done
 }
 
-trap '/bin/rm -rf \"$sauvegarde_rm\"' EXIT # to handle signals
+trap '/bin/rm -rf "$sauvegarde_rm"' EXIT # to handle signals
 
